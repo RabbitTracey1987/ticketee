83f108be05b70079e5fd63ef91dd10607c5e4aeb
RabbitTracey1987 <traceyw0224@gmail.com>
2017-07-18 21:59:47 -0400

    Projects can not be deleted

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,9 @@ class ProjectsController < ApplicationController
 	def index
 		@projects = Project.all
 	end
+	def edit
+		@project = Project.find(params[:id])
+	end
 	
 	def new
 		@project = Project.new
@@ -19,6 +22,24 @@ class ProjectsController < ApplicationController
 	def show
 		@project = Project.find(params[:id])
 	end
+	def  update 
+		@project = Project.find(params[:id])
+		if @project.update(project_params)
+			flash[:notice] = 'Project has been updated'
+			redirect_to @project
+		else
+			flash.now[:alert] = "Project has not been updated"
+			render "edit"
+		end
+	end
+
+	def destroy
+		@project = Project.find(params[:id])
+		@project.destroy
+		flash[:notice]="Project has been deleted"
+		redirect_to projects_path
+	end
+	
 	private
 	def project_params
 		params.require(:project).permit(:name,:description)
